43cd284fd4e4ed9cb30e0bdd9537b3b1fedc68f1
STUDENT 31 <STUDENT 31@DESKTOP-9G47GDA>
2026-05-09 18:27:55 +0800

    vvvv

--- VallejosLibraryNowAPI/Program.cs
+++ VallejosLibraryNowAPI/Program.cs
@@ -8,8 +8,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-//var port = Environment.GetEnvironmentVariable("port") ?? "8080";
-//app.Urls.Add($"http://+:{port});"
+var port = Environment.GetEnvironmentVariable("port") ?? "8080";
+app.Urls.Add($"http://+:{port}");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
